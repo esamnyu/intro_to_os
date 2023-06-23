8f373c3e3d68834735e1d974110b511896026df1
esamnyu <105534679+esamnyu@users.noreply.github.com>
2023-06-23 18:32:32 -0400

    When a process is completed (i.e., when handle_process_completion_srtp is called), we need to find the next process to be executed. It's logical to start looking from the beginning of the queue and choose the process with the shortest remaining burst time. If the queue is empty, we will return NULLPCB indicating no process is ready to be executed.

--- cpu.c
+++ cpu.c
@@ -24,6 +24,36 @@ struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queu
     }
 }
 
+struct PCB handle_process_completion_srtp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp) {
+    struct PCB handle_process_completion_srtp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp) {
+    if (*queue_cnt == 0) {
+        return NULLPCB;  // Return NULLPCB if the ready queue is empty
+    }
+
+    int next_process_index = 0;
+    for (int i = 0; i < *queue_cnt; i++) {
+        if (ready_queue[i].remaining_bursttime < ready_queue[next_process_index].remaining_bursttime) {
+            next_process_index = i;
+        }
+    }
+
+    struct PCB next_process = ready_queue[next_process_index];
+
+    // Shift the remaining processes in the ready queue
+    for (int i = next_process_index; i < *queue_cnt - 1; i++) {
+        ready_queue[i] = ready_queue[i + 1];
+    }
+    (*queue_cnt)--;
+
+    next_process.execution_starttime = timestamp;
+    next_process.execution_endtime = timestamp + next_process.remaining_bursttime;
+
+    return next_process;
+}
+
+}
+
+
 struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp) {
     struct PCB completed_process;
     completed_process = ready_queue[0];
