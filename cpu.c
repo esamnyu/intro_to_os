d8dc9e962dc8de592a93abe1b8892a1d03bcbda5
esamnyu <105534679+esamnyu@users.noreply.github.com>
2023-06-23 15:52:02 -0400

    CHanged to operator > again

--- cpu.c
+++ cpu.c
@@ -14,7 +14,7 @@ struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queu
         (*queue_cnt)++;
         return new_process;
     } else {
-        if (new_process.process_priority < current_process.process_priority) { // New process preempts the current one
+        if (new_process.process_priority > current_process.process_priority) { // New process preempts the current one
             current_process.remaining_bursttime -= timestamp - current_process.execution_starttime;
             current_process.execution_endtime = timestamp + current_process.remaining_bursttime;
             ready_queue[*queue_cnt] = current_process; // current_process is added back to ready_queue
