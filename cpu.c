7089c0aaf25d616c98370eafa592d3d0d90b7f2c
esamnyu <105534679+esamnyu@users.noreply.github.com>
2023-06-23 14:20:39 -0400

    Attempting to solve: handle_process_arrival_pp (0/1) Test Failed: 'Missing PCB in ready_queue for test 1 [1,[92 chars]: 6]' != 'Passed All Tests' - Missing PCB in ready_queue for test 1 [1,1,4,1,0, 3, 8].  Here is the ready queue. [PID: 2 AT: 2 TBT: 3 EST: 0 EET: 0 RBT: 3 Priority: 6] + Passed All Tests

--- cpu.c
+++ cpu.c
@@ -5,25 +5,27 @@
 const struct PCB NULLPCB = {0, 0, 0, 0, 0, 0, 0};
 
 struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp) {
+    new_process.execution_starttime = timestamp;
+    new_process.remaining_bursttime = new_process.total_bursttime;
+
     if (current_process.process_id == 0) { // Assuming process_id = 0 means it's the NULLPCB
-        new_process.execution_starttime = timestamp;
         new_process.execution_endtime = timestamp + new_process.total_bursttime;
-        new_process.remaining_bursttime = new_process.total_bursttime;
-        ready_queue[*queue_cnt] = new_process;
+    } else {
+        ready_queue[*queue_cnt] = current_process;
         (*queue_cnt)++;
-        return new_process;
-    }
-    else {
-        new_process.remaining_bursttime = new_process.total_bursttime;
-        ready_queue[*queue_cnt] = new_process;
-        (*queue_cnt)++;
-        if (new_process.process_priority < current_process.process_priority) {
-            return new_process;
-        } else {
-            return current_process;
+        if (new_process.process_priority < current_process.process_priority) { // New process preempts the current one
+            current_process.remaining_bursttime -= timestamp - current_process.execution_starttime;
+            current_process.execution_endtime = timestamp + current_process.remaining_bursttime;
+            new_process.execution_endtime = timestamp + new_process.total_bursttime;
+        } else { // Current process continues
+            new_process.execution_endtime = current_process.execution_endtime + new_process.total_bursttime;
         }
     }
+    ready_queue[*queue_cnt] = new_process;
+    (*queue_cnt)++;
+    return new_process;
 }
+
 
 struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp) {
     struct PCB completed_process;
