ec9670c6fd97aaad46161136f61c80b66b229e8a
esamnyu <105534679+esamnyu@users.noreply.github.com>
2023-06-28 11:29:46 -0400

    You have a structure PTE defined for the page table entry and the function process_page_access_lfu which is already declared in the header file, hence it needs to be implemented.
    Please note that in your PTE structure, the is_valid attribute represents whether the page is in memory or not.

--- virtual.c
+++ virtual.c
@@ -94,58 +94,63 @@ int count_page_faults_fifo(struct PTE page_table[TABLEMAX], int table_cnt, int r
     return faults;
 }
 
-int process_page_access_lfu(struct PTE page_table[TABLEMAX], int *table_cnt, int page_number, int frame_pool[POOLMAX], int *frame_cnt, int current_timestamp) {
-    if (page_table[page_number].is_valid) {
+int process_page_access_lfu(struct PTE page_table[TABLEMAX], int *table_cnt, int page_number, int frame_pool[POOLMAX], int *frame_cnt, int current_timestamp){
+    
+    // If page already in memory
+    if(page_table[page_number].is_valid){
         page_table[page_number].last_access_timestamp = current_timestamp;
         page_table[page_number].reference_count++;
-
         return page_table[page_number].frame_number;
-    } else {
-        if (*frame_cnt > 0) {
-            int frame = frame_pool[--(*frame_cnt)];
+    }
 
-            page_table[page_number].is_valid = 1;
-            page_table[page_number].frame_number = frame;
-            page_table[page_number].arrival_timestamp = current_timestamp;
-            page_table[page_number].last_access_timestamp = current_timestamp;
-            page_table[page_number].reference_count = 1;
+    // If there are any free frames
+    if(*frame_cnt > 0){
+        --(*frame_cnt);
+        int free_frame = frame_pool[*frame_cnt];
+        page_table[page_number].is_valid = 1;
+        page_table[page_number].frame_number = free_frame;
+        page_table[page_number].arrival_timestamp = current_timestamp;
+        page_table[page_number].last_access_timestamp = current_timestamp;
+        page_table[page_number].reference_count = 1;
+        return free_frame;
+    }
 
-            return frame;
-        } else {
-            int lfu_page = -1;
-            int min_reference_count = INT_MAX;
-            int earliest_arrival_timestamp = INT_MAX;
-
-            for (int i = 0; i < *table_cnt; i++) {
-                if (page_table[i].is_valid) {
-                    if (page_table[i].reference_count < min_reference_count) {
-                        min_reference_count = page_table[i].reference_count;
-                        earliest_arrival_timestamp = page_table[i].arrival_timestamp;
-                        lfu_page = i;
-                    } else if (page_table[i].reference_count == min_reference_count && page_table[i].arrival_timestamp < earliest_arrival_timestamp) {
-                        earliest_arrival_timestamp = page_table[i].arrival_timestamp;
-                        lfu_page = i;
-                    }
-                }
+    // Page replacement
+    int min_count = INT_MAX;
+    int oldest_timestamp = INT_MAX;
+    int replace_index = -1;
+    
+    // Find the least frequently used page
+    for(int i = 0; i < *table_cnt; ++i){
+        if(page_table[i].is_valid && page_table[i].reference_count <= min_count){
+            if(page_table[i].reference_count < min_count || (page_table[i].reference_count == min_count && page_table[i].arrival_timestamp < oldest_timestamp)){
+                min_count = page_table[i].reference_count;
+                oldest_timestamp = page_table[i].arrival_timestamp;
+                replace_index = i;
             }
-
-            int frame = page_table[lfu_page].frame_number;
-
-            page_table[lfu_page].is_valid = 0;
-            page_table[lfu_page].frame_number = -1;
-            page_table[lfu_page].arrival_timestamp = 0;
-            page_table[lfu_page].last_access_timestamp = 0;
-            page_table[lfu_page].reference_count = 0;
-
-            page_table[page_number].is_valid = 1;
-            page_table[page_number].frame_number = frame;
-            page_table[page_number].arrival_timestamp = current_timestamp;
-            page_table[page_number].last_access_timestamp = current_timestamp;
-            page_table[page_number].reference_count = 1;
-
-            return frame;
         }
     }
+    
+    if(replace_index == -1){
+        // Couldn't find a page to replace
+        return -1;
+    }
+
+    // Replace the least frequently used page
+    int replace_frame = page_table[replace_index].frame_number;
+    page_table[replace_index].is_valid = 0;
+    page_table[replace_index].frame_number = -1;
+    page_table[replace_index].arrival_timestamp = 0;
+    page_table[replace_index].last_access_timestamp = 0;
+    page_table[replace_index].reference_count = 0;
+
+    page_table[page_number].is_valid = 1;
+    page_table[page_number].frame_number = replace_frame;
+    page_table[page_number].arrival_timestamp = current_timestamp;
+    page_table[page_number].last_access_timestamp = current_timestamp;
+    page_table[page_number].reference_count = 1;
+
+    return replace_frame;
 }
 
 int process_page_access_lru(struct PTE page_table[TABLEMAX], int *table_cnt, int page_number, int frame_pool[POOLMAX], int *frame_cnt, int current_timestamp) {
