74cfaa88642e360950c223c04819fa1c2032391b
esamnyu <105534679+esamnyu@users.noreply.github.com>
2023-06-29 16:47:32 -0400

    This function does the following:
    It initializes the count of page faults (page_faults) and the current timestamp (current_timestamp).
    
    Then, it iterates over each page number in the reference string. For each page number, it does the following:
    
    It stores the current frame count before processing the page (prev_frame_count).
    
    It calls the process_page_access_fifo function to process the page access. If a page fault occurs during this process, the frame count will decrease.
    
    After processing the page, it checks if the frame count decreased. If it did, it means a page fault occurred, so it increments the page_faults count.
    
    It increments the current_timestamp for the next iteration.
    
    Finally, after processing all the pages in the reference string, it returns the total number of page faults.

--- virtual.c
+++ virtual.c
@@ -50,54 +50,29 @@ int process_page_access_fifo(struct PTE page_table[TABLEMAX], int *table_cnt, in
 
 
 int count_page_faults_fifo(struct PTE page_table[TABLEMAX], int table_cnt, int reference_string[REFERENCEMAX], int reference_cnt, int frame_pool[POOLMAX], int frame_cnt) {
-    int faults = 0;
-    int timestamp = 0;
+    int page_faults = 0;
+    int current_timestamp = 1;
 
     for (int i = 0; i < reference_cnt; i++) {
         int page_number = reference_string[i];
-        timestamp++;
 
-        if (page_table[page_number].is_valid) {
-            page_table[page_number].last_access_timestamp = timestamp;
-            page_table[page_number].reference_count++;
-        } else {
-            faults++;
+        // Previous frame count before processing the page
+        int prev_frame_count = frame_cnt;
 
-            if (frame_cnt > 0) {
-                int frame = frame_pool[--frame_cnt];
+        // Process the page access
+        process_page_access_fifo(page_table, &table_cnt, page_number, frame_pool, &frame_cnt, current_timestamp);
 
-                page_table[page_number].is_valid = 1;
-                page_table[page_number].frame_number = frame;
-                page_table[page_number].arrival_timestamp = timestamp;
-                page_table[page_number].last_access_timestamp = timestamp;
-                page_table[page_number].reference_count = 1;
-            } else {
-                int oldest_page_number = 0;
-                for (int j = 1; j < table_cnt; j++) {
-                    if (page_table[j].is_valid && page_table[j].arrival_timestamp < page_table[oldest_page_number].arrival_timestamp) {
-                        oldest_page_number = j;
-                    }
-                }
-
-                int frame = page_table[oldest_page_number].frame_number;
-
-                page_table[oldest_page_number].is_valid = 0;
-                page_table[oldest_page_number].frame_number = -1;
-                page_table[oldest_page_number].arrival_timestamp = -1;
-                page_table[oldest_page_number].last_access_timestamp = -1;
-                page_table[oldest_page_number].reference_count = 0;
-
-                page_table[page_number].is_valid = 1;
-                page_table[page_number].frame_number = frame;
-                page_table[page_number].arrival_timestamp = timestamp;
-                page_table[page_number].last_access_timestamp = timestamp;
-                page_table[page_number].reference_count = 1;
-            }
+        // If frame count decreased, it means a page fault occurred
+        if (frame_cnt < prev_frame_count) {
+            page_faults++;
         }
+
+        current_timestamp++;
     }
 
-    return faults;
+    return page_faults;
 }
+
 
 int process_page_access_lfu(struct PTE page_table[TABLEMAX], int *table_cnt, int page_number, int frame_pool[POOLMAX], int *frame_cnt, int current_timestamp){
     
@@ -313,42 +288,42 @@ int count_page_faults_lfu(struct PTE page_table[TABLEMAX], int table_cnt, int re
     return faults;
 }
 
-// int main() {
-//     // Initialize page table
-//     struct PTE page_table[TABLEMAX];
-//     int table_cnt = TABLEMAX;
+int main() {
+    // Initialize page table
+    struct PTE page_table[TABLEMAX];
+    int table_cnt = TABLEMAX;
 
-//     // Assume all pages are initially not in memory
-//     for (int i = 0; i < table_cnt; i++) {
-//         page_table[i].is_valid = 0;
-//         page_table[i].frame_number = -1;
-//         page_table[i].arrival_timestamp = -1;
-//         page_table[i].last_access_timestamp = -1;
-//         page_table[i].reference_count = -1;
-//     }
+    // Assume all pages are initially not in memory
+    for (int i = 0; i < table_cnt; i++) {
+        page_table[i].is_valid = 0;
+        page_table[i].frame_number = -1;
+        page_table[i].arrival_timestamp = -1;
+        page_table[i].last_access_timestamp = -1;
+        page_table[i].reference_count = -1;
+    }
 
-//     // Initialize frame pool with some frames
-//     int frame_pool[POOLMAX] = {10, 20, 30, 40};
-//     int frame_cnt = 4;
+    // Initialize frame pool with some frames
+    int frame_pool[POOLMAX] = {10, 20, 30, 40};
+    int frame_cnt = 4;
 
-//     // Reference string and count
-//     int reference_string[REFERENCEMAX] = {1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5};
-//     int reference_cnt = 12;
+    // Reference string and count
+    int reference_string[REFERENCEMAX] = {1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5};
+    int reference_cnt = 12;
 
-//     // Run count_page_faults_fifo
-//     int faults = count_page_faults_fifo(page_table, table_cnt, reference_string, reference_cnt, frame_pool, frame_cnt);
+    // Run count_page_faults_fifo
+    int faults = count_page_faults_fifo(page_table, table_cnt, reference_string, reference_cnt, frame_pool, frame_cnt);
 
-//     // Check the result
-//     printf("Page faults: %d\n", faults);
-//     assert(faults == 8);
+    // Check the result
+    printf("Page faults: %d\n", faults);
+    assert(faults == 8);
 
-//     return 0;
-// }
-// // The assert statement will abort the program if the number of faults is not 8, printing an error message similar to the one you're seeing.
+    return 0;
+}
+// The assert statement will abort the program if the number of faults is not 8, printing an error message similar to the one you're seeing.
 
-// // Please replace the reference_string and reference_cnt with the actual sequence of page accesses and length used in your test case, as I don't know what specific sequence causes the discrepancy between the expected and actual number of page faults.
+// Please replace the reference_string and reference_cnt with the actual sequence of page accesses and length used in your test case, as I don't know what specific sequence causes the discrepancy between the expected and actual number of page faults.
 
-// // Run the test case and see if it passes. If it does not pass, use print statements or a debugger to find out what's happening inside count_page_faults_fifo and process_page_access_fifo.
+// Run the test case and see if it passes. If it does not pass, use print statements or a debugger to find out what's happening inside count_page_faults_fifo and process_page_access_fifo.
 
 
 
