896ff504dd829b0ad07adf5a131b92276ba3e081
esamnyu <105534679+esamnyu@users.noreply.github.com>
2023-06-30 16:39:20 -0400

    Implemented stub functions

--- virtual.c
+++ virtual.c
@@ -127,6 +127,25 @@ int count_page_faults_lfu(struct PTE page_table[TABLEMAX], int table_cnt, int re
     return faults;
 }
 
+int process_page_access_fifo(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp) {
+    // This is just a stub function. Replace with real implementation later.
+    return 0;
+}
+
+int process_page_access_lru(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp) {
+    // This is just a stub function. Replace with real implementation later.
+    return 0;
+}
+
+int count_page_faults_lru(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt) {
+    // This is just a stub function. Replace with real implementation later.
+    return 0;
+}
+
+int process_page_access_lfu(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp) {
+    // This is just a stub function. Replace with real implementation later.
+    return 0;
+}
 
 
 // int main() {
