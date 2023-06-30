16594dd6eb602e453e1fd227b83f53e877136e61
esamnyu <105534679+esamnyu@users.noreply.github.com>
2023-06-30 16:25:15 -0400

    This function begins by iterating through the reference string. If the referenced page is already in memory, it updates the last access timestamp and increments the reference count. If the page is not in memory and there are available frames, it adds the page to memory using a frame from the pool and increments the fault count. If the page is not in memory and there are no available frames, it finds the oldest page in memory, removes it, adds the new page using the freed frame, and increments the fault count. It then increments the timestamp for the next iteration. The function returns the total number of faults.

--- virtual.c
+++ virtual.c
@@ -4,6 +4,66 @@
 #include <assert.h>
 #include <stdbool.h>
 
+int count_page_faults_fifo(struct PTE page_table[TABLEMAX], int table_cnt, int reference_string[REFERENCEMAX], int reference_cnt, int frame_pool[POOLMAX], int frame_cnt) {
+    int faults = 0;
+    int current_timestamp = 1;
+
+    for (int i = 0; i < reference_cnt; i++) {
+        int page_number = reference_string[i];
+
+        // Page is in memory
+        if (page_table[page_number].is_valid) {
+            page_table[page_number].last_access_timestamp = current_timestamp;
+            page_table[page_number].reference_count++;
+        } 
+        // Page is not in memory and frames are available
+        else if (frame_cnt > 0) {
+            page_table[page_number].is_valid = 1;
+            page_table[page_number].frame_number = frame_pool[0];
+            page_table[page_number].arrival_timestamp = current_timestamp;
+            page_table[page_number].last_access_timestamp = current_timestamp;
+            page_table[page_number].reference_count = 1;
+
+            // Shift the frames in the pool to the left
+            for (int j = 0; j < frame_cnt - 1; j++) {
+                frame_pool[j] = frame_pool[j+1];
+            }
+
+            frame_cnt--;
+            faults++;
+        } 
+        // Page is not in memory and no frames are available (page replacement needed)
+        else {
+            int oldest_page = 0;
+            for (int j = 0; j < TABLEMAX; j++) {
+                if (page_table[j].is_valid && page_table[j].arrival_timestamp < page_table[oldest_page].arrival_timestamp) {
+                    oldest_page = j;
+                }
+            }
+
+            // Remove the oldest page
+            int freed_frame = page_table[oldest_page].frame_number;
+            page_table[oldest_page].is_valid = 0;
+            page_table[oldest_page].frame_number = -1;
+            page_table[oldest_page].arrival_timestamp = -1;
+            page_table[oldest_page].last_access_timestamp = -1;
+            page_table[oldest_page].reference_count = -1;
+
+            // Insert the new page
+            page_table[page_number].is_valid = 1;
+            page_table[page_number].frame_number = freed_frame;
+            page_table[page_number].arrival_timestamp = current_timestamp;
+            page_table[page_number].last_access_timestamp = current_timestamp;
+            page_table[page_number].reference_count = 1;
+
+            faults++;
+        }
+
+        current_timestamp++;
+    }
+
+    return faults;
+}
 
 
 int count_page_faults_lfu(struct PTE page_table[TABLEMAX], int table_cnt, int reference_string[REFERENCEMAX], int reference_cnt, int frame_pool[POOLMAX], int frame_cnt) {
